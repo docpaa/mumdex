80bf92e428ac8d160804019436bfc254027ff44b
Peter Andrews <paa@drpa.us>
2018-02-08 12:43:58 -0500

    deleted some scripts

--- unstable/transfer_to_nygc.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-scp -pr $1 andmac:transfer/$1
-
-ssh -A andmac scp -r transfer/$1 andrewsp-488@mumdel.nygenome.org:$1
-
-
--- unstable/tunnel_to_nygc.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-echo do locally: ssh -p 2229 -R 2222:localhost:22 andrewsp-488@127.0.0.1
-
-ssh -L 2229:mumdel.nygenome.org:22 andmac
-
